f475b661ad9626e78db09263a051ae3e6d1f8835
Laxmi Sah <laxmisah464@gmail.com>
2026-07-16 19:56:02 +0545

    Add files via upload

---
+++ opera.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+cout<<"Enter a number:";//This is called insertion operator
+cin>>n;//This is called extraction operator
+cout<<"You entered: "<<n<<endl;
+}
