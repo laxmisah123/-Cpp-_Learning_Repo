d215d3db84a86eeb71d39fea5cb215a4779b14b6
Laxmi Sah <laxmisah464@gmail.com>
2026-07-18 13:33:11 +0545

    Add files via upload

---
+++ head.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"world.h"
+//There are two types of header files
+//System header files: It comes with the compiler
+//User defined header files: It is written by the programmer
+using namespace std;
+int main()
+{
+    cout<<"Hello World"<<endl;
+    return 0;
+}
